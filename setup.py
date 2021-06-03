ec90f57aaa79c634ac0c1a5abc1604437c77f5ef
gully <igully@gmail.com>
2021-06-03 09:39:31 -0500

    :hammer: Initial commit of setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+
+setuptools.setup(
+    name="volum",
+    version="0.0.1",
+    author="gully",
+    author_email="igully@gmail.com",
+    description="A Python package for working with precomputed synthetic spectral models",
+    long_description="A Python package for working with precomputed synthetic spectral models such as PHOENIX and Sonora-Bobcat",
+    long_description_content_type="text/markdown",
+    url="https://github.com/gully/volum",
+    install_requires=["numpy", "scipy", "astropy", "specutils", "pandas", "matplotlib"],
+    packages=setuptools.find_packages(where="src"),
+    package_dir={"": "src"},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
