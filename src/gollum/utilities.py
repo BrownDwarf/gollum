573435c7306a56c359856844ca6c9911cac079c4
Sujay Shankar <sujay.shankar@utexas.edu>
2022-07-01 15:12:49 +0530

    Move truncate method into utilities due to code duplication

--- src/gollum/utilities.py
+++ src/gollum/utilities.py
@@ -1,4 +1,5 @@
 from copy import deepcopy
+from numpy import array
 from specutils.spectra import Spectrum1D
 
 
@@ -32,4 +33,48 @@ def apply_numpy_mask(spec, mask):
         mask=spec.mask[mask] if spec.mask else None,
         wcs=None,
         meta=deepcopy(spec.meta) if spec.meta else None,
+    )
+
+def truncate(grid, wavelength_range=None, data=None):
+    """Truncate the wavelength range of the grid
+
+    Parameters
+    ----------
+    wavelength_range: list or tuple
+        A pair of values that denote the shortest and longest wavelengths
+        for truncating the grid.
+    data: Spectrum1D-like
+        A spectrum to which this method will match the wavelength limits
+        
+    Returns
+    -------
+    truncated_spectrum: Spectrum1D-like
+        The spectrum after being truncated to the given wavelength range
+    """
+    fiducial_spec = deepcopy(grid[0])
+    wavelength_units = fiducial_spec.wavelength.unit
+    flux_units = fiducial_spec.flux.unit
+
+    if data and not wavelength_range:
+        wavelength_range = (
+            fiducial_spec.wavelength.value.min() * wavelength_units,
+            fiducial_spec.wavelength.value.max() * wavelength_units,
+        )
+
+    shortest_wavelength, longest_wavelength = wavelength_range
+
+    wavelengths, fluxes = [], []
+    for spectrum in grid:
+        mask = (spectrum.wavelength > shortest_wavelength) & (
+            spectrum.wavelength < longest_wavelength
+        )
+        wavelengths.append(spectrum.wavelength.value[mask])
+        fluxes.append(spectrum.flux.value[mask])
+
+    assert fluxes and wavelengths
+
+    return grid.__class__(
+        flux=array(fluxes) * flux_units,
+        spectral_axis=array(wavelengths) * wavelength_units,
+        meta=grid.meta,
     )
